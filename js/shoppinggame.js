00ca9680cc522859b6a2d9ae17b7c945ec92ed31
Mirian Fonkam <mirianfonkam@outlook.com>
2021-04-15 22:50:45 -0300

    Update shoppinggame.js
    completed module 3

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -91,7 +91,7 @@ const loadProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.keys() to load the property names of the Product object in to prodKeys array here
-        let prodKeys = [];
+        let prodKeys = Object.keys(new Product());
 
         let iterator_obj = map.entries();
 
@@ -101,7 +101,7 @@ const loadProducts = (map, prodId) => {
                 const value = item[1];
 
                 // Create and assign an instance of Product to prodObj here
-                let prodObj;
+                let prodObj = new Product();
 
                 if (prodObj != undefined && prodObj != null) {
                     for (let i = 0; i < prodKeys.length; i++) {
@@ -135,7 +135,7 @@ const loadMagicProducts = (map, prodId) => {
     let a = new Array();
     try {
         // Call Object.key() to load the property names of the MagicProduct object in to magProdKeys array here
-        let magProdKeys = [];
+        let magProdKeys = Object.keys(new MagicProduct());
 
         let iterator_obj = map.entries();
 
@@ -143,9 +143,9 @@ const loadMagicProducts = (map, prodId) => {
             for (let item of iterator_obj) {
                 const key = item[0];
                 const value = item[1];
-
+ 
                 // Create and assign an instance of MagicProduct to prodObj here
-                let magProdObj;
+                let magProdObj = new MagicProduct();
 
                 if (magProdObj != undefined && magProdObj != null) {
                     for (let i = 0; i < magProdKeys.length; i++) {
